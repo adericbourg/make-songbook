403fbf36ac00316c3cabde35f186cf4f2eaaef52
Alban Dericbourg <alban@dericbourg.net>
2018-09-11 16:10:15 +0200

    Write library to a dummy text songbook
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- makesongbook/songbook_printer.py
+++ makesongbook/songbook_printer.py
@@ -2,9 +2,22 @@
 # -*- coding: utf-8 -*-
 """Songbook printing."""
 
+import os
+
 from makesongbook.song_library import Library
 
 
 def print_library(library: Library, target: str):
     """Print library into a songbook."""
-    print("Printing library from:", target)
+    songbook_file = os.path.join(target, "songbook.txt")
+    print("Printing library to:", target)
+    if os.path.exists(songbook_file):
+        os.remove(songbook_file)
+    with open(songbook_file, 'a') as songbook:
+        for artist in library.artists:
+            songbook.write("===========\n".format(artist.name))
+            songbook.write("==== {} ====\n".format(artist.name))
+            songbook.write("===========\n".format(artist.name))
+            for song in artist.songs:
+                songbook.write("-- {}\n".format(song.title))
+                songbook.write(song.content)
