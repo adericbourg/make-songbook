a59a840684b776f4ca4989f2ba0fcc871173ce74
Alban Dericbourg <alban@dericbourg.net>
2018-09-11 16:22:47 +0200

    Init structure for output template
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- makesongbook/songbook_printer.py
+++ makesongbook/songbook_printer.py
@@ -4,7 +4,11 @@
 
 import os
 
+import jinja2
+
 from makesongbook.song_library import Library
+
+_TEMPLATE_FILE = "songbook.template.txt"
 
 
 def print_library(library: Library, target: str):
@@ -13,11 +17,12 @@ def print_library(library: Library, target: str):
     print("Printing library to:", target)
     if os.path.exists(songbook_file):
         os.remove(songbook_file)
+
+    template_loader = jinja2.FileSystemLoader(searchpath="./template")
+    template_env = jinja2.Environment(loader=template_loader)
+    template = template_env.get_template(_TEMPLATE_FILE)
+
+    output = template.render({"library": library})
+
     with open(songbook_file, 'a') as songbook:
-        for artist in library.artists:
-            songbook.write("===========\n".format(artist.name))
-            songbook.write("==== {} ====\n".format(artist.name))
-            songbook.write("===========\n".format(artist.name))
-            for song in artist.songs:
-                songbook.write("-- {}\n".format(song.title))
-                songbook.write(song.content)
+        songbook.write(output)
