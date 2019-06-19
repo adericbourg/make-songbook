bd71245782f4ae36c696553c3a7ecac818877cc0
Alban Dericbourg <alban@dericbourg.net>
2019-06-19 16:41:00 +0200

    Prompt user to provide the root of their library
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let _ = App::new("make-songbook")
+        .version("0.1.0")
+        .about("make-songbook builds an ePub to bring your songs and tablatures into an eInk reader")
+        .author("Alban Dericbourg <alban@dericbourg.net>")
+        .arg(Arg::with_name("library")
+            .help("Sets the root location of your library")
+            .required(true))
+        .get_matches();
 }
